0c40d36f0e9e4caa98cc2ac5da264dfef24900de
Alex Dunn <dunn.alex@gmail.com>
2015-08-27 09:37:07 -0700

    new prefix: C-c C-h [SEMVER MAJOR]
    Is it reaaaally semver major? Are the keybindings part of the API?
    Maybe not but this is a breaking change for users, so there.

--- README.md
+++ README.md
@@ -12,31 +12,31 @@ Emacs minor mode for editing [Homebrew](http://brew.sh) formulae.
 
 ## keys and commands
 
-The command prefix is <kbd>C-c h</kbd>.  These are the commands currently mapped to it:
+The command prefix is <kbd>C-c C-h</kbd>.  These are the commands currently mapped to it:
 
-- <kbd>C-c h f</kbd>: Download the source file(s) for the formula
+- <kbd>C-c C-h f</kbd>: Download the source file(s) for the formula
   in the current buffer.
 
-- <kbd>C-c h u</kbd>: Download and unpack the source file(s) for the formula
+- <kbd>C-c C-h u</kbd>: Download and unpack the source file(s) for the formula
   in the current buffer.
 
-- <kbd>C-c h i</kbd>: Install the formula in the current buffer.
+- <kbd>C-c C-h i</kbd>: Install the formula in the current buffer.
 
-- <kbd>C-c h r</kbd>: Uninstall the formula in the current buffer.
+- <kbd>C-c C-h r</kbd>: Uninstall the formula in the current buffer.
 
-- <kbd>C-c h t</kbd>: Run the test for the formula in the current buffer.
+- <kbd>C-c C-h t</kbd>: Run the test for the formula in the current buffer.
 
-- <kbd>C-c h a</kbd>: Audit the formula in the current buffer.
+- <kbd>C-c C-h a</kbd>: Audit the formula in the current buffer.
 
-- <kbd>C-c h c</kbd>: Open a dired buffer in the Homebrew cache
+- <kbd>C-c C-h c</kbd>: Open a dired buffer in the Homebrew cache
   (default `/Library/Caches/Homebrew`).
 
-- <kbd>C-c h d</kbd>: Add `depends_on` lines for the specified
+- <kbd>C-c C-h d</kbd>: Add `depends_on` lines for the specified
   formulae.  Call with one prefix (<kbd>C-u</kbd>) argument to make
   them build-time dependencies; call with two (<kbd>C-u C-u</kbd>) for
   run-time.
 
-- <kbd>C-c h p</kbd>: Insert Python `resource` blocks (requires poet,
+- <kbd>C-c C-h p</kbd>: Insert Python `resource` blocks (requires poet,
   installed with `pip install homebrew-pypi-poet`).
 
 ## custom variables
--- homebrew-mode.el
+++ homebrew-mode.el
@@ -38,31 +38,31 @@
 
 ;; ## keys and commands
 
-;; The command prefix is <kbd>C-c h</kbd>.  These are the commands currently mapped to it:
+;; The command prefix is <kbd>C-c C-h</kbd>.  These are the commands currently mapped to it:
 
-;; - <kbd>C-c h f</kbd>: Download the source file(s) for the formula
+;; - <kbd>C-c C-h f</kbd>: Download the source file(s) for the formula
 ;;   in the current buffer.
 
-;; - <kbd>C-c h u</kbd>: Download and unpack the source file(s) for the formula
+;; - <kbd>C-c C-h u</kbd>: Download and unpack the source file(s) for the formula
 ;;   in the current buffer.
 
-;; - <kbd>C-c h i</kbd>: Install the formula in the current buffer.
+;; - <kbd>C-c C-h i</kbd>: Install the formula in the current buffer.
 
-;; - <kbd>C-c h r</kbd>: Uninstall the formula in the current buffer.
+;; - <kbd>C-c C-h r</kbd>: Uninstall the formula in the current buffer.
 
-;; - <kbd>C-c h t</kbd>: Run the test for the formula in the current buffer.
+;; - <kbd>C-c C-h t</kbd>: Run the test for the formula in the current buffer.
 
-;; - <kbd>C-c h a</kbd>: Audit the formula in the current buffer.
+;; - <kbd>C-c C-h a</kbd>: Audit the formula in the current buffer.
 
-;; - <kbd>C-c h c</kbd>: Open a dired buffer in the Homebrew cache
+;; - <kbd>C-c C-h c</kbd>: Open a dired buffer in the Homebrew cache
 ;;   (default `/Library/Caches/Homebrew`).
 
-;; - <kbd>C-c h d</kbd>: Add `depends_on` lines for the specified
+;; - <kbd>C-c C-h d</kbd>: Add `depends_on` lines for the specified
 ;;   formulae.  Call with one prefix (<kbd>C-u</kbd>) argument to make
 ;;   them build-time dependencies; call with two (<kbd>C-u C-u</kbd>) for
 ;;   run-time.
 
-;; - <kbd>C-c h p</kbd>: Insert Python `resource` blocks (requires poet,
+;; - <kbd>C-c C-h p</kbd>: Insert Python `resource` blocks (requires poet,
 ;;   installed with `pip install homebrew-pypi-poet`).
 
 ;; ## custom variables
@@ -95,7 +95,7 @@
 
 ;; Most of the keymap stuff discovered through studying flycheck.el,
 ;; so ty lunaryorn.
-(defcustom homebrew-mode-keymap-prefix (kbd "C-c h")
+(defcustom homebrew-mode-keymap-prefix (kbd "C-c C-h")
   "Prefix for homebrew-mode key bindings."
   :group 'homebrew-mode
   :type 'string
