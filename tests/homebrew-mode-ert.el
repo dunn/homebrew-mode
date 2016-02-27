66ac685fc23a14897b22ca4f4db247ee9f85cc38
Alex Dunn <dunn.alex@gmail.com>
2016-02-27 09:19:42 -0800

    update (C) years, include copy of license

--- tests/homebrew-mode-ert.el
+++ tests/homebrew-mode-ert.el
@@ -1,3 +1,22 @@
+;;; homebrew-mode-ert.el --- ERT tests for homebrew-mode
+
+;; Copyright (C) 2015, 2016 Alex Dunn
+
+;; This file is part of homebrew-mode
+
+;; homebrew-mode is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (require 'ert)
 (require 'homebrew-mode)
 
